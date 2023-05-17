ecac6719b2cab2cd1474d4da12297aafa870bb08
tatsenko-m <tatsenko.m@yandex.ru>
2023-05-17 14:37:35 +0300

    feat: добавить закрытие InfoTooltip по Esc и клику на оверлей

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -5,8 +5,33 @@ import failImage from '../images/fail-image.svg';
 function InfoTooltip({ isOpen, onClose, isAuthSuccess }) {
   const backgroundImage = isAuthSuccess ? `url(${successImage})` : `url(${failImage})`;
 
+  React.useEffect(() => {
+    const handleEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' && isOpen) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleEscKeyDown);
+    
+      return () => {
+        document.removeEventListener('keydown', handleEscKeyDown);
+      };
+    }
+  }, [isOpen, onClose]);
+
+  const handleOverlayMouseDown = (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      onClose();
+    }
+  };
+
   return (
-    <div className={`popup ${isOpen && "popup_opened"}`}>
+    <div 
+      className={`popup ${isOpen && "popup_opened"}`} 
+      onMouseDown={handleOverlayMouseDown}
+    >
       <div className="popup__container">
       <button
           className="popup__close-button"
