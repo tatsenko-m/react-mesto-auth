a6be8bc4f531158ed90b4b7403a923dbb0e2aa89
tatsenko-m <tatsenko.m@yandex.ru>
2023-05-17 14:29:39 +0300

    style: изменить стили иконки статуса регистрации

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -3,6 +3,8 @@ import successImage from '../images/success-image.svg';
 import failImage from '../images/fail-image.svg';
 
 function InfoTooltip({ isOpen, onClose, isAuthSuccess }) {
+  const backgroundImage = isAuthSuccess ? `url(${successImage})` : `url(${failImage})`;
+
   return (
     <div className={`popup ${isOpen && "popup_opened"}`}>
       <div className="popup__container">
@@ -11,11 +13,10 @@ function InfoTooltip({ isOpen, onClose, isAuthSuccess }) {
           type="button"
           onClick={onClose}
       ></button>
-      <img
-        src={isAuthSuccess ? successImage : failImage} 
-        alt="Иконка статуса" 
+      <div
         className="popup__info-tooltip-icon"
-      />
+        style={{ backgroundImage }}
+      ></div>
       <p className="popup__info-tooltip-text">
         {isAuthSuccess ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.'}
       </p>
